a131bd5e4427ccb68440d6e02c1d06ea64fc48e4
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2024-10-14 12:12:38 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import RollingSlider from "./components/Slider/RollingSlider";
 import Modal from "./components/Modals/Modal";
 import Testimonials from "./components/Testimonials/Testimonials";
-import BrandList from "./components/List/BrandList";
 
 export default function Home() {
   return (
     <main className="md:pt-[10vh] sm:pt-[5vh]">
       <RollingSlider/>
-      <BrandList/>
       <Testimonials/>
       <Modal/>
     </main>
