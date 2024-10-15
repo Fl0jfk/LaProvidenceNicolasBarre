30f123b293e1a80d528888be16898d1deca2bdda
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2024-10-15 14:27:46 +0200

    adjust padding

--- src/app/components/News/News.tsx
+++ src/app/components/News/News.tsx
@@ -8,7 +8,7 @@ export default function News() {
     const { news, error } = useData();
     if (error){ return <div>Error: {error}</div>}
     return (
-        <section className="w-full p-4 max-w-[1200px] mx-auto flex flex-col">
+        <section className="w-full px-8 max-w-[1200px] mx-auto flex flex-col">
             <h3 className="text-3xl uppercase">Les Actualités</h3>
             <h4 className="text-3xl">Du groupe scolaire</h4>
             <div className="mt-4 grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
