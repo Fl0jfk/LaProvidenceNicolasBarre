2562e05922fb214ab176cc05ea619e9ed82ea288
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2024-10-15 11:36:18 +0200

    add title

--- src/app/components/Banner/Banner.tsx
+++ src/app/components/Banner/Banner.tsx
@@ -1,6 +1,6 @@
 export default function Banner(){
     return (
-        <section className="bg-pink-600 text-white flex flex-col items-center justify-center p-6 gap-2 w-full max-w-[1018px] mx-auto">
+        <section className="bg-pink-600 text-white flex flex-col items-center justify-center p-6 gap-2 w-full max-w-[1017px] mx-auto">
             <h2 className="uppercase text-4xl">Portes Ouvertes</h2>
             <p className="text-2xl">Samedi 16 Novembre 2024 de 8h30 à 12h30 sur RDV (tél : 02-32-86-50-90)</p>
         </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,13 @@ export default function Home() {
     <main className="md:pt-[10vh] sm:pt-[5vh]">
       <h1 className="my-4 w-[90%] text-center text-4xl sm:mt-28 md:mt-20 max-w-[1200px] mx-auto">Bienvenue à La Providence Nicolas Barré</h1>
       <Video/>
+      <section className="w-full max-w-[1017px] flex flex-col items-center justify-center text-3xl uppercase shadow-md p-4 mx-auto">
+        <div className="flex gap-2 w-full items-center justify-center">
+          <h2 className="text-[#FBB800]">La Providence</h2>
+          <h2 className="text-[#18AAE2]">Nicolas Barré</h2>
+        </div>
+        <p className="text-[#E3097B] font-semibold">Une passeport pour l'avenir</p>
+      </section>
       <RollingSlider/>
     </main>
   );
