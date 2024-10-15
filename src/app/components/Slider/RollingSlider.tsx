e3024958720ceecc1b92b4db5687c7b8f279f279
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2024-10-15 11:55:48 +0200

    add text

--- src/app/components/Slider/RollingSlider.tsx
+++ src/app/components/Slider/RollingSlider.tsx
@@ -6,12 +6,17 @@ import Slider from "./Slider";
 export default function RollingSlider (){
     const data = useData();
     return (
-        <>
+        <section className="max-w-[1200px] w-full self-center mx-auto mt-4">
+            <div className="p-4 flex flex-col gap-1">
+                <h3 className="text-3xl"><span>3</span> établissements</h3>
+                <p className="font-light">École, collège, lycée, trois directions, mais un seul projet éducatif, une seule tutelle, un même esprit, que nous vous invitons à découvrir.</p>
+                <p className="font-light">Acquisition des savoirs, éducation à la liberté, annonce de l'Evangile, trois objectifs qui nous semblent majeurs. Trois établissements dont nous percevrez l'unité et les différences car, naturellement, on ne propose ni les mêmes règles, ni les mêmes méthodes d'enseignement à l'école, au collège ou au lycée.</p>
+            </div>
             {data.categories[0] &&
-                <section id="slideshow" className="max-w-[1200px] w-full self-center mx-auto">
+                <div id="slideshow">
                     <Slider props={data.categories}/>
-                </section>
+                </div>
             }
-        </>
+        </section>
     )
 }
